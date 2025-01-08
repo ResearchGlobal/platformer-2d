8caef71614372d00cb1256f48d4e60e32c801e5d
David <dbehmoaras@gmail.com>
2025-01-08 15:02:35 -0500

    bullet collisions working

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -13,9 +13,9 @@ public partial class Projectile : RigidBody2D
 		timer.Timeout += () => QueueFree();
 	}
 
-	public void OnBodyEntered(Node2D node)
+	public void OnBodyEntered(Node node)
 	{
-		GD.Print("Projectile body entered called");
+		GD.Print("Projectile body entered called " + node.Name);
 		if (node.IsInGroup("enemy_npc"))
 		{
 			GD.Print("projectile collided with " + node.Name);
